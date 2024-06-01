88b721a30c09688b787f1a8d2a5c7925d0e8d2a8
Lucas Su <lijie.su93@gmail.com>
2024-06-01 12:49:00 +0930

    prisma + move to step2-design(pass img id)

--- DEV.md
+++ DEV.md
@@ -1,36 +1,51 @@
+## Study Notes
+
+### 03-basic design
+- npx next, shadui-cn
+- lib/utils.ts: export `cn` used to solve and combine classnames by twMerge and clsx.
+```typescript
+const className = cn('bg-blue-500', 'text-white', { 'hover:bg-blue-700': true, 'p-4': false })
+// p-4 will not be applied
+```
+- MaxWidthWrapper
 
 
-### 03-npx next, shadui-cn, got lib/utils.ts, MaxWidthWrapper
+### 04-framer-motion
+- install framer-motion, configure `tailwind.config.ts`
 
-
-
-### 04-npm i framer-motion
-
-
-
-### 08-npm i react-dropzone - "https://react-dropzone.js.org/#src"
+### 08-react-dropzone
+- https://react-dropzone.js.org/#src
 - Need to set the layout in order to make it full view screen height.
+- shadcn progress by `npx shadcn-ui@latest add progress`
 
-### 08-use shadcn progress by "npx shadcn-ui@latest add progress"
-
-### 08-use uploadthings and add 'secret' + 'app_id' to .env "npm install uploadthing @uploadthing/react"
+### 08-uploadthings
+- `npm install uploadthing @uploadthing/react`
+- add 'secret' + 'app_id' to `.env` 
 - follow the doc page (add 2 files in app/api/uploadthing)
 - add 1 another configuration file in `lib/` folder.
 - install `zod` to verify the uploaded file meet our standard(format, size)
 - changed core.ts and upload/page a lot, main idea is to pass the configId from the server side to the client side.
 - install shadcn-toast, follow the doc: need to add <Toast/> at the app/layout.tsx.
 
-### 08-implement Steps: open /upload/layout, put it above children.
+### 08-Steps
 
+- open /upload/layout, put it above `{children}`.
 - use `usePathname()` to get the current path name.
 - doing lots of checks
 
-### 08-implement dabase
+### 08-init database
 - register neon-tech
-- install prisma @prisma/client
-- npx prisma init --> to gen prisma folder
-- add db/index.ts, to make sure only 1 prisma client is created during dev phase by checking 'process.env.NODE_ENV'. (not that important)
+- `npx install prisma @prisma/client`
+- `npx prisma init` --> to gen prisma folder
+- add `db/index.ts`, to make sure only 1 prisma client is created during dev phase by checking `process.env.NODE_ENV`. (not that important)
 - add configure/design/page.tsx (the second step)
 
+### 09-configuration
+- init `schema.prisma`
+  - `cuid()` the unique id but faster than `uuid()`, `@id` means the main key.
+  - `  configuration   Configuration @relation(fields: [configurationId], references: [id])` fields is the outer key in current model, it related to model Configuration with its id.
+  - `npm prisma db push` - this will push our schema onto Neon-tables
+  - `npm i sharp@0.32.6` - used to deal with img to get its info, but need to change img into binary buffer first.`const buffer = await res.arrayBuffer();`
+  
 
 
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,48 +1,53 @@
-import { createUploadthing, type FileRouter } from 'uploadthing/next'
-import { z } from 'zod'
-import sharp from 'sharp'
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { z } from "zod";
+import sharp from "sharp";
+import { db } from "@/db";
 // import { db } from '@/db'
 
-const f = createUploadthing()
+const f = createUploadthing();
 
 export const ourFileRouter = {
-  imageUploader: f({ image: { maxFileSize: '4MB' } })
+  imageUploader: f({ image: { maxFileSize: "4MB" } })
     .input(z.object({ configId: z.string().optional() }))
     .middleware(async ({ input }) => {
-      return { input }
+      return { input };
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      const { configId } = metadata.input
+      const { configId } = metadata.input;
 
-      // const res = await fetch(file.url)
-      // const buffer = await res.arrayBuffer()
+      // new added after db is created
+      const res = await fetch(file.url);
 
-      // const imgMetadata = await sharp(buffer).metadata()
-      // const { width, height } = imgMetadata
+      const buffer = await res.arrayBuffer(); // changed to binary data
+      const imgMetadata = await sharp(buffer).metadata(); // feed binary buffer to lib sharp
 
-      // if (!configId) {
-      //   const configuration = await db.configuration.create({
-      //     data: {
-      //       imageUrl: file.url,
-      //       height: height || 500,
-      //       width: width || 500,
-      //     },
-      //   })
+      const { width, height } = imgMetadata;
 
-        return { configId }
-      // } else {
-      //   const updatedConfiguration = await db.configuration.update({
-      //     where: {
-      //       id: configId,
-      //     },
-      //     data: {
-      //       croppedImageUrl: file.url,
-      //     },
-      //   })
+      // if config is null, create a new one
+      if (!configId) {
+        const configuration = await db.configuration.create({
+          data: {
+            imageUrl: file.url,
+            height: height || 500,
+            width: width || 500,
+          },
+        });
 
-      //   return { configId: updatedConfiguration.id }
-      // }
+        return { configId: configuration.id };
+      } else {
+        // in step 2, update img to cropped one
+        const updatedConfiguration = await db.configuration.update({
+          where: {
+            id: configId,
+          },
+          data: {
+            croppedImageUrl: file.url,
+          },
+        });
+
+        return { configId: updatedConfiguration.id };
+      }
     }),
-} satisfies FileRouter
+} satisfies FileRouter;
 
-export type OurFileRouter = typeof ourFileRouter
+export type OurFileRouter = typeof ourFileRouter;
